12b727501da1a0a9bbbce522c8d5c77c3549febb
Neilketchum <daipayanh@gmail.com>
2024-10-22 18:23:12 -0700

    commiting

--- portfoliobackend/schemaTypes/index.js
+++ portfoliobackend/schemaTypes/index.js
@@ -3,4 +3,6 @@ import {about} from './about'
 import { brand } from "./brands"
 import { contact } from "./contact"
 import { skills } from "./skills"
-export const schemaTypes = [testimonial,about,brand,contact,skills]
+import { workExperience } from "./workExperience"
+import { works } from "./works"
+export const schemaTypes = [testimonial,about,brand,contact,skills,workExperience,works]
